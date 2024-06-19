ce4236fa27768443ac2839fc6a08add744b2bbe2
schultztimothy <schultz.timothy52@gmail.com>
2024-06-18 18:15:30 -0600

    feat: remaining notification logic

--- app/components/MinimalHeader.tsx
+++ app/components/MinimalHeader.tsx
@@ -1,6 +1,7 @@
 // --- React methods
 import React, { useMemo } from "react";
 import { Notifications } from "./Notifications";
+import { OnchainSidebar } from "./OnchainSidebar";
 
 type MinimalHeaderProps = {
   className?: string;
@@ -17,17 +18,21 @@ const getAssets = () => {
 
 const MinimalHeader = ({ className }: MinimalHeaderProps): JSX.Element => {
   const assets = useMemo(() => getAssets(), []);
+  const [showSidebar, setShowSidebar] = React.useState(false);
 
   return (
-    <div className={`flex justify-between h-16 ${className}`}>
-      <div className="flex items-center">
-        <img className="" src={assets.gitcoinLogo} alt="Gitcoin Logo" />
-        <img className="mx-3 md:mx-6" src={assets.logoLine} alt="Logo Line" />
-        <img className="h-8" src={assets.passportLogo} alt="Passport Logo" />
-        <div className="ml-3 text-2xl text-color-1">Passport</div>
+    <>
+      <OnchainSidebar isOpen={showSidebar} onClose={() => setShowSidebar(false)} />
+      <div className={`flex justify-between h-16 ${className}`}>
+        <div className="flex items-center">
+          <img className="" src={assets.gitcoinLogo} alt="Gitcoin Logo" />
+          <img className="mx-3 md:mx-6" src={assets.logoLine} alt="Logo Line" />
+          <img className="h-8" src={assets.passportLogo} alt="Passport Logo" />
+          <div className="ml-3 text-2xl text-color-1">Passport</div>
+        </div>
+        <Notifications setShowSidebar={() => setShowSidebar(true)} />
       </div>
-      <Notifications />
-    </div>
+    </>
   );
 };
 
